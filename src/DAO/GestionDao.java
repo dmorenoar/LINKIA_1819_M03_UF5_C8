80572b3fce8ac8551a73da2e8da481337d06f693
Daniel Moreno <Daniel Moreno@danimorenopc>
2018-12-13 12:12:28 +0100

    Ejemplo de uso de JDBC con JAVA

--- src/DAO/GestionDao.java
+++ src/DAO/GestionDao.java
@@ -5,13 +5,17 @@
  */
 package DAO;
 
+import Excepciones.ExcepcionLigaPokemon;
 import Modelos.Entrenador;
+import Modelos.Pokemon;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -55,12 +59,11 @@ public class GestionDao {
         
     }
     
-    public void insertarEntrenador(Entrenador e) throws SQLException{
+    public void insertarEntrenador(Entrenador e) throws SQLException, ExcepcionLigaPokemon{
         
         if(existeEntrenador(e)){
-            //Gestionar con excepciones.
-            System.out.println("Entrenador existe");
-        }else{
+            throw new ExcepcionLigaPokemon("ERROR: Entrenador ya existe en la BDD");
+        }
             
             String insert = "Insert into Entrenador (nombre, telefono, sexo, edad, experiencia, especialidad) values(?,?,?,?,?,?)";
             
@@ -75,36 +78,112 @@ public class GestionDao {
             ps.executeUpdate();
             
             ps.close();
+        
+    }
+    
+    //Método para borrar un entrenador
+    public void deleteEntrenador(Entrenador e) throws ExcepcionLigaPokemon, SQLException{
+        
+        if(!existeEntrenador(e)){
+            throw new ExcepcionLigaPokemon("ERROR: Entrenador no existe en la BDD");
         }
+        
+        String delete = "Delete from Entrenador where nombre='" + e.getNombre() + "'";
+        
+        Statement st = conexion.createStatement();
+        st.executeUpdate(delete);
+        st.close();
+        
     }
     
     
+    public List<Entrenador> getAllEntrenadores() throws SQLException{
+        List<Entrenador> entrenadores = new ArrayList();
+     
+        String select = "Select * From Entrenador";
+        Statement st = conexion.createStatement();
+        ResultSet rs = st.executeQuery(select);
+        
+        while(rs.next()){
+            //nombre, telefono, sexo, edad, experiencia, especialidad
+            Entrenador e = new Entrenador();
+            e.setNombre(rs.getString("nombre"));
+            e.setTelefono(rs.getString("telefono"));
+            e.setEdad(rs.getInt("edad"));
+            e.setExperiencia(rs.getInt("experiencia"));
+            e.setEspecialidad(rs.getString("especialidad"));
+            entrenadores.add(e);
+            
+        }
+        
+        rs.close();
+        st.close();
+        
+    
+        return entrenadores;
+        
+    }
     
     
+    /*Gestión de Pokemons*/
+    public void altaPokemon(Pokemon p ) throws SQLException, ExcepcionLigaPokemon{
+        
+        if(existePokemon(p)){
+            throw new ExcepcionLigaPokemon("ERROR: Pokemon ya existe en la BDD");
+        }
+        
+            // nombre tipo; fuerza; entrenador;
+        
+            
+            String insert = "Insert into Pokemon (nombre, tipo, fuerza, entrenador) values(?,?,?,?)";
+            
+            PreparedStatement ps = conexion.prepareStatement(insert);
+            ps.setString(1, p.getNombre());
+            ps.setString(2, p.getTipo());
+            ps.setDouble(3, p.getFuerza());
+            ps.setString(4, p.getEntrenador().getNombre());
+            ps.executeUpdate();
+            
+            ps.close();
+        
+        
+    }
+    
+    public boolean existePokemon (Pokemon p) throws SQLException{
+        String select = "Select * from Pokemon where nombre ='" + p.getNombre() + "'";
+    
+        Statement st = conexion.createStatement();
+        ResultSet rs = st.executeQuery(select);
+        
+        boolean existe = false;
+        
+        if(rs.next()){
+            existe = true;
+        }
+        
+        rs.close();
+        st.close();
+        
+        return existe;
+        
+    }
+   
+    public void subirFuerzaPokemon(Pokemon p) throws SQLException, ExcepcionLigaPokemon{
+        
+        //Comprobar si existe el pokemon
+        if(!existePokemon(p)){
+            throw new ExcepcionLigaPokemon("ERROR: Pokemon no existe en la BDD");
+        }
+        
+        String update = "Update Pokemon set fuerza = ? where nombre= '" + p.getNombre() + "'";
+        PreparedStatement ps = conexion.prepareStatement(update);
+        ps.setDouble(1, p.getFuerza()); //ps.setDouble(1, 99);
+        ps.executeUpdate();
+        ps.close();
+        
+    }
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
     public void conectar() throws SQLException{
         
         String url= "jdbc:mysql://localhost:3306/ligapokemon";
@@ -121,5 +200,4 @@ public class GestionDao {
         }
         
     }
-    
 }
--- src/MainApp/LINKIA_1819_M03_UF5_C8.java
+++ src/MainApp/LINKIA_1819_M03_UF5_C8.java
@@ -6,8 +6,12 @@
 package MainApp;
 
 import DAO.GestionDao;
+import Excepciones.ExcepcionLigaPokemon;
 import Modelos.Entrenador;
+import Modelos.Pokemon;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -18,52 +22,119 @@ public class LINKIA_1819_M03_UF5_C8 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws SQLException {
-        
+    public static void main(String[] args) {
+
         GestionDao ligaDAO = new GestionDao();
-        
-        //Establecer al conexion
-        conectar(ligaDAO);
-        //Testeando insertar un entrenador
-        Entrenador ash = new Entrenador("Ash", "+3412345", "Hombre", 2, 2, "Eléctrico");
-        insertarEntrenador(ligaDAO, ash);
-        
-        //Testeando insertar un entrenador repetido
-        
-        //Testeando mostrar la lista de entrenadores
-        
-        //Testeando insertar un pokemon
-        
-        //Testeando insertar un pokemon con un entrenador que no existe
-        
-        //Testeando borrar un pokemon
-        
-        //Testeando buscar un entrenador por nombre
-        
-        //Testeando subir la fuerza de un pokemon
-        
-        //Desconectar
-        desconectar(ligaDAO);
-        
-        
+
+        try {
+
+            //Establecer al conexion
+            conectar(ligaDAO);
+            
+            //Testeando insertar un entrenador
+            Entrenador ash = new Entrenador("Ash", "+3412345", "Hombre", 2, 2, "Eléctrico");
+            insertarEntrenador(ligaDAO, ash);
+
+            //Testeando insertar un entrenador repetido
+            //El existeEntrenador nos comprueba esta fase
+            insertarEntrenador(ligaDAO, ash);
+
+            //Testeando borrar un entrenador
+            //borrarEntrenador(ligaDAO, ash);
+
+            //Testeando mostrar la lista de entrenadores
+            Entrenador brock = new Entrenador("Brock", "+3412345", "Hombre",15 , 58, "Roca");
+            insertarEntrenador(ligaDAO, brock);
+            
+            System.out.println("Testeando mostrar lista entrenadores");
+            for (Entrenador e : selectAllEntrenadores(ligaDAO)) {
+                System.out.println(e);
+            }
+            
+            
+            //Testeando insertar un pokemon, ash);
+            Pokemon p = new Pokemon("Pikachu", "Eléctrico", 20, ash);
+            insertarPokemon(ligaDAO,p);
+            
+            //Testeando insertar un pokemon con un entrenador que no existe
+            
+            
+            //Testeando borrar un pokemon
+            //Testeando buscar un entrenador por nombre
+            //Testeando subir la fuerza de un pokemon
+            p.setFuerza(p.getFuerza() + 1);
+            subirFuerzaPokemon(ligaDAO, p);
+            
+            
+            //Desconectar
+            desconectar(ligaDAO);
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
     }
-    
-    public static void conectar(GestionDao ligaDAO) throws SQLException{
-        System.out.println("Conectando...");      
+
+    public static void conectar(GestionDao ligaDAO) throws SQLException {
+        System.out.println("Conectando...");
         ligaDAO.conectar();
     }
-    
-    
-    public static void desconectar(GestionDao ligaDAO) throws SQLException{
+
+    public static void desconectar(GestionDao ligaDAO) throws SQLException {
         System.out.println("Desconectando...");
         ligaDAO.desconectar();
         System.out.println("Desconexión realizada.");
     }
+
+    public static void insertarEntrenador(GestionDao ligaDao, Entrenador e) throws SQLException {
+
+        try {
+            ligaDao.insertarEntrenador(e);
+            System.out.println("Entrenador insertado correctamente");
+        } catch (ExcepcionLigaPokemon ex) {
+            System.out.println(ex.getMessage());
+        }
+
+    }
     
-    public static void insertarEntrenador(GestionDao ligaDao, Entrenador e) throws SQLException{
-        ligaDao.insertarEntrenador(e);
-        System.out.println("Entrenador insertado correctamente");
+    public static void borrarEntrenador(GestionDao ligaDao, Entrenador e) throws SQLException {
+
+        try {
+            ligaDao.deleteEntrenador(e);
+            System.out.println("Entrenador borrado correctamente");
+        } catch (ExcepcionLigaPokemon ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+    public static List<Entrenador> selectAllEntrenadores(GestionDao ligaDao) throws SQLException{
+        List<Entrenador> entrenadores = new ArrayList();
+        
+        entrenadores = ligaDao.getAllEntrenadores();
+
+        return entrenadores;   
+    }
+    
+    public static void insertarPokemon(GestionDao ligaDao, Pokemon p) throws SQLException{
+        
+        try {
+            ligaDao.altaPokemon(p);
+            System.out.println("Pokemon:" + p.getNombre() + " dado de alta");
+        } catch (ExcepcionLigaPokemon ex) {
+            System.out.println(ex.getMessage());
+        }
     }
     
     
+    public static void subirFuerzaPokemon(GestionDao ligaDao, Pokemon p) throws SQLException{
+        
+        try {
+            ligaDao.subirFuerzaPokemon(p);
+            System.out.println("Pokemon:" + p.getNombre() + " subido con fuerza " + p.getFuerza());
+        } catch (ExcepcionLigaPokemon ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+    }
+
 }
